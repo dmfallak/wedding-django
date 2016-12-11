4dfe2941690838c142b270233e659a9d735adb23
Devin Fallak <dmfallak@gmail.com>
2016-12-11 16:52:45 -0500

    First commit

---
+++ wedding/models.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Guest
+  invitee = models.CharField(max_length=256)
+  attending_max = models.IntegerField(default=0)
+  attending_num = models.IntegerField(default=0)
+  guest_names = models.CharField(max_length=1024)
+  entree1 = models.CharField(max_length=128)
+  entree2 = models.CharField(max_length=128)
+  hotel = models.BooleanField(default=False)
+  shuttle_to_time = models.IntegerField(default=0)
+  shuttle_from_time = models.IntegerField(default=0)
+  attending = models.BooleanField(default=False)
+  responded = models.BooleanField(default=False)
+  
