6d1be840762ce1379ba137358048e217bead9647
Devin Fallak <dmfallak@gmail.com>
2016-12-11 18:55:58 -0500

    Guest model hooked up
    Can retrieve URL params

--- wedding/models.py
+++ wedding/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Guest
+class Guest(models.Model):
   invitee = models.CharField(max_length=256)
   attending_max = models.IntegerField(default=0)
   attending_num = models.IntegerField(default=0)
@@ -14,4 +14,6 @@ class Guest
   shuttle_from_time = models.IntegerField(default=0)
   attending = models.BooleanField(default=False)
   responded = models.BooleanField(default=False)
-  
+
+  def __str__(self):
+    return self.invitee
--- wedding/views.py
+++ wedding/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import pprint
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+  pp = pprint.PrettyPrinter()
+  pp.pprint(request.GET.get('bla'))
+  return HttpResponse("Hello, world. You're at the polls index.")
